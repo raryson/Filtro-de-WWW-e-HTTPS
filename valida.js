c81d453877fafdbdd44d3aeec2efaabc4cf634ea
Raryson Pereira Rost <raryson.rost@gmail.com>
2017-05-11 10:46:11 -0300

    alteracoes feitas

--- valida.js
+++ valida.js
@@ -3,14 +3,30 @@ function retiraProtocolo(url){
     
     const reGexHttp = new RegExp("^(http|https)://", "i")
     const reGexHttpWithWWW = new RegExp("^(http|https)://www.", "i")
+     const reGexWww = new RegExp("^www.", "i")
 
     if(reGexHttpWithWWW.test(url)){
         let filtred = url.replace(reGexHttpWithWWW, '')
+        console.log(filtred.substring(filtred.length -1, filtred.length))
+        if(filtred.substring(filtred.length -1, filtred.length)  === '/'){
+            return  filtred.substring(0, filtred.length - 1)
+        }
         return filtred
     }
 
     if(reGexHttp.test(url)){
         let filtred = url.replace(reGexHttp, '')
+        if(filtred.substring(filtred.length -1, filtred.length)  === '/'){
+            return  filtred.substring(0, filtred.length - 1)
+        }
+        return filtred
+    }
+
+    if(reGexWww.test(url)){
+        let filtred = url.replace(reGexWww, '')
+        if(filtred.substring(filtred.length -1, filtred.length) === '/'){
+            return  filtred.substring(0, filtred.length - 1)
+        }
         return filtred
     }
 
